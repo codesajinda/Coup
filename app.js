e1fb74ea8221756fc4488c4567cef2c202d493ef
codesajinda <csmasterpiece@gmail.com>
2015-01-24 14:20:18 -0600

    Added support to create rooms

--- app.js
+++ app.js
@@ -3,113 +3,128 @@ var app = express();
 app.use(express.static(__dirname));
 
 var server = app.listen(process.env.PORT || 3000);
-/*app.get('/', function(req,res) {
-  //res.sendFile(__dirname  + '/index.html');
-    //res.sendFile(__dirname  + '/css/bootstrap.min.css');
-	//res.render('/index.html');
-});  */  
 app.get('/');
 var io = require('socket.io').listen(server)
-
+var rooms = [];
+var usernames ={};
+var currentRoom = '';
 io.sockets.on('connection', function (socket) {
+  socket.on('AddUser', function(username, room){
+   if(username!="" && room != ""){
+		currentRoom = room.toString();
+		// store the username in the socket session for this client		
+		socket.username = username;
+		// store the room name in the socket session for this client
+		socket.room = currentRoom;
+		// add the client's username to the global list
+		usernames[username] = username;
+		rooms.push(currentRoom);
+		// send client to room 1
+		socket.join(currentRoom);
+		// echo to client they've connected
+		socket.emit('ConnectionStatus', 'SERVER', 'you have connected to ' + currentRoom);
+		// echo to room  that a person has connected to their room
+		socket.broadcast.to(currentRoom).emit('ConnectionStatus', 'SERVER', username + ' has connected to this room');
+	}
+  });
   socket.on('SetupPlayer1', function (data) {
-    io.sockets.emit('SetupPlayer2', data);
+    io.sockets.in(socket.room).emit('SetupPlayer2', data);
   });
   socket.on('BuildForeignAidBlockLayout', function (data) {
-    io.sockets.emit('BuildForeignAidBlockLayout', data);
+    io.sockets.in(socket.room).emit('BuildForeignAidBlockLayout', data);
   }); 
   socket.on('BuildStealAsCaptainLayout', function (data) {
-    io.sockets.emit('BuildStealAsCaptainLayout', data);
+    io.sockets.in(socket.room).emit('BuildStealAsCaptainLayout', data);
   });
   socket.on('PlayerBlockedForeignAid', function (data) {
-    io.sockets.emit('PlayerBlockedForeignAid', data);
+    io.sockets.in(socket.room).emit('PlayerBlockedForeignAid', data);
   });
   socket.on('PlayerUnBlockedForeignAid', function (data) {
-    io.sockets.emit('PlayerUnBlockedForeignAid', data);
+    io.sockets.in(socket.room).emit('PlayerUnBlockedForeignAid', data);
   });  
   socket.on('PlayerBlockedDuke', function (data) {
-    io.sockets.emit('PlayerBlockedDuke', data);
+    io.sockets.in(socket.room).emit('PlayerBlockedDuke', data);
   });
   socket.on('PlayerBlockedForeignAidDuke', function (data) {
-    io.sockets.emit('PlayerBlockedForeignAidDuke', data);
+    io.sockets.in(socket.room).emit('PlayerBlockedForeignAidDuke', data);
   });
   socket.on('PlayerUnBlockedDuke', function (data) {
-    io.sockets.emit('PlayerUnBlockedDuke', data);
+    io.sockets.in(socket.room).emit('PlayerUnBlockedDuke', data);
   }); 
   socket.on('CannotStealAsCaptainNoIncome', function (data) {
-    io.sockets.emit('CannotStealAsCaptainNoIncome', data);
+    io.sockets.in(socket.room).emit('CannotStealAsCaptainNoIncome', data);
   }); 
   socket.on('PlayerUnBlockedStealAsCaptain', function (data) {
-    io.sockets.emit('PlayerUnBlockedStealAsCaptain', data);
+    io.sockets.in(socket.room).emit('PlayerUnBlockedStealAsCaptain', data);
   });
   socket.on('PlayerHasDuke', function (data) {
-    io.sockets.emit('PlayerHasDuke', data);
+    io.sockets.in(socket.room).emit('PlayerHasDuke', data);
   });
   socket.on('PlayerBluffDuke', function (data) {
-    io.sockets.emit('PlayerBluffDuke', data);
+    io.sockets.in(socket.room).emit('PlayerBluffDuke', data);
   });
   socket.on('PlayerSaidBluffCaptain', function (data) {
-    io.sockets.emit('PlayerSaidBluffCaptain', data);
+    io.sockets.in(socket.room).emit('PlayerSaidBluffCaptain', data);
   });
   socket.on('PlayerSaidBluffAssassin', function (data) {
-    io.sockets.emit('PlayerSaidBluffAssassin', data);
+    io.sockets.in(socket.room).emit('PlayerSaidBluffAssassin', data);
   });
   socket.on('PlayerHasAssassin', function (data) {
-    io.sockets.emit('PlayerHasAssassin', data);
+    io.sockets.in(socket.room).emit('PlayerHasAssassin', data);
   });
   socket.on('PlayerBluffCaptain', function (data) {
-    io.sockets.emit('PlayerBluffCaptain', data);
+    io.sockets.in(socket.room).emit('PlayerBluffCaptain', data);
   });
   socket.on('PlayerBluffContessa', function (data) {
-    io.sockets.emit('PlayerBluffContessa', data);
+    io.sockets.in(socket.room).emit('PlayerBluffContessa', data);
   });
   socket.on('PlayerSaidBluffContessa', function (data) {
-    io.sockets.emit('PlayerSaidBluffContessa', data);
+    io.sockets.in(socket.room).emit('PlayerSaidBluffContessa', data);
   });  
   socket.on('GameOver', function (data) {
-    io.sockets.emit('GameOver', data);
+    io.sockets.in(socket.room).emit('GameOver', data);
   });
   socket.on('PlayerSaidHasCaptain', function (data) {
-    io.sockets.emit('PlayerSaidHasCaptain', data);
+    io.sockets.in(socket.room).emit('PlayerSaidHasCaptain', data);
   });
   socket.on('PlayerSaidHasContessa', function (data) {
-    io.sockets.emit('PlayerSaidHasContessa', data);
+    io.sockets.in(socket.room).emit('PlayerSaidHasContessa', data);
   });
     socket.on('UpdateDisabled', function (data) {
-    io.sockets.emit('UpdateDisabled', data);
+    io.sockets.in(socket.room).emit('UpdateDisabled', data);
   });
   socket.on('PlayerStealHasAmbassador', function (data) {
-    io.sockets.emit('PlayerStealHasAmbassador', data);
+    io.sockets.in(socket.room).emit('PlayerStealHasAmbassador', data);
   });
   socket.on('PlayerSaidBluffAmbassador', function (data) {
-    io.sockets.emit('PlayerSaidBluffAmbassador', data);
+    io.sockets.in(socket.room).emit('PlayerSaidBluffAmbassador', data);
   });
   socket.on('DukeBlockLayout', function (data) {
-    io.sockets.emit('DukeBlockLayout', data);
+    io.sockets.in(socket.room).emit('DukeBlockLayout', data);
   });
   socket.on('RemoveCard', function (data) {
-    io.sockets.emit('RemoveCard', data);
+    io.sockets.in(socket.room).emit('RemoveCard', data);
   });
    socket.on('CoupPlayer', function (data) {
-    io.sockets.emit('CoupPlayer', data);
+    io.sockets.in(socket.room).emit('CoupPlayer', data);
   });
   socket.on('AnnounceRemovedCard', function (data) {
-    io.sockets.emit('AnnounceRemovedCard', data);
+    io.sockets.in(socket.room).emit('AnnounceRemovedCard', data);
   });
   socket.on('ExchangeAmbassadorLayout', function (data) {
-    io.sockets.emit('ExchangeAmbassadorLayout', data);
+    io.sockets.in(socket.room).emit('ExchangeAmbassadorLayout', data);
   });
   socket.on('PlayerBlockedAmbassador', function (data) {
-    io.sockets.emit('PlayerBlockedAmbassador', data);
+    io.sockets.in(socket.room).emit('PlayerBlockedAmbassador', data);
   });
   socket.on('PlayerUnBlockedAmbassador', function (data) {
-    io.sockets.emit('PlayerUnBlockedAmbassador', data);
+    io.sockets.in(socket.room).emit('PlayerUnBlockedAmbassador', data);
   });
   socket.on('PlayerHasAmbassador', function (data) {
-    io.sockets.emit('PlayerHasAmbassador', data);
+    io.sockets.in(socket.room).emit('PlayerHasAmbassador', data);
   });
   socket.on('AssassinPlayer', function (data) {
-    io.sockets.emit('AssassinPlayer', data);
+    io.sockets.in(socket.room).emit('AssassinPlayer', data);
   });  
 });
 
